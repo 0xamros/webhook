95a525420a17fff9303c640cba02379d50b5e634
khaled <khaled.amrosy.fci@gmail.com>
2020-05-28 14:08:00 +0300

    iintial commit

---
+++ src/main/java/com/example/service/PersonService.java
@@ -0,0 +1,46 @@
+package com.example.service;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.stream.Stream;
+
+import org.springframework.stereotype.Service;
+
+import com.example.model.Person;
+
+@Service
+public class PersonService {
+	ArrayList<Person> persons = new ArrayList<Person>();
+	public PersonService() {
+		Person p = new Person();
+		p.setId("1");
+		p.setAge(21);
+		p.setFirstName("khaled");
+		p.setLastName("khaled");
+		persons.add(p);
+
+		p = new Person();
+		p.setId("2");
+		p.setAge(25);
+		p.setFirstName("shalaby");
+		p.setLastName("shalaby");
+		persons.add(p);
+		
+		p = new Person();
+		p.setId("3");
+		p.setAge(25);
+		p.setFirstName("basem");
+		p.setLastName("salama");
+		persons.add(p);
+	}
+	public Person getPerson(String id) {
+		for(Person person:persons) {
+			if(person.getId().equalsIgnoreCase(id)) return person;
+		}
+	    return null;
+	}
+	public ArrayList<Person> getAll() {
+		return persons;
+	}
+}
+
