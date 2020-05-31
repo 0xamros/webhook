b3f3fc1da34cc44353795b9a3a65a5c357f022f6
khalednoh <khaled.amrosy.fci@gmail.com>
2020-05-31 22:08:50 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("webhook");
 		p.setLastName("testing");
 		persons.add(p);
+		
+		p = new Person();
+		p.setId("5");
+		p.setAge(40);
+		p.setFirstName("email");
+		p.setLastName("notification");
+		persons.add(p);
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
