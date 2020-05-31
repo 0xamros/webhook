ae46cdc85db802ea9474a430883722a8e27e1441
khalednoh <khaled.amrosy.fci@gmail.com>
2020-05-31 22:41:02 +0300

    testing email notification

--- src/main/java/com/example/Demo1Application.java
+++ src/main/java/com/example/Demo1Application.java
@@ -16,6 +16,7 @@ public class Demo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 		// testing webhook
+		// testing email notification
 	}
 
 }
