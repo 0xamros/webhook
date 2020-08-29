e4bb3b7a26a948d3a33337ff030a3dba3a7388d8
khalednoh <khaled.amrosy.fci@gmail.com>
2020-08-29 23:45:57 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -33,6 +33,13 @@ public class PersonService {
 		p.setLastName("Salama");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("4");
+		p.setAge(25);
+		p.setFirstName("AbdulHalim");
+		p.setLastName("Mahmoud");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
